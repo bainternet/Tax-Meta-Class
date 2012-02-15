8e9441d2c2acf7328d1300ca49fd9706aa27f21b
bainternet <admin@bainternet.info>
2012-02-15 13:15:44 +0200

    fixed validation callback

--- Tax-meta-class/Tax-meta-class.php
+++ Tax-meta-class/Tax-meta-class.php
@@ -9,7 +9,7 @@
  * This class is derived from My-Meta-Box (https://github.com/bainternet/My-Meta-Box script) which is 
  * a class for creating custom meta boxes for WordPress. 
  * 
- * @version 1.1
+ * @version 1.2
  * @copyright 2012 
  * @author Ohad Raz (email: admin@bainternet.info)
  * @link http://en.bainternet.info
@@ -1071,7 +1071,7 @@ class Tax_Meta_Class {
 
 			// Validate meta value
 			if ( class_exists( 'Tax_Meta_Validate' ) && method_exists( 'Tax_Meta_Validate', $field['validate_func'] ) ) {
-				$new = call_user_func( array( 'at_Meta_Box_Validate', $field['validate_func'] ), $new );
+				$new = call_user_func( array( 'Tax_Meta_Validate', $field['validate_func'] ), $new );
 			}
 			
 			//skip on Paragraph field

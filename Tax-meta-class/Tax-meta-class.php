5f05649875e3c1b94d64604cfcb03779147c8688
bainternet <admin@bainternet.info>
2012-08-25 19:09:52 +0300

    updated version number

--- Tax-meta-class/Tax-meta-class.php
+++ Tax-meta-class/Tax-meta-class.php
@@ -9,7 +9,7 @@
  * This class is derived from My-Meta-Box (https://github.com/bainternet/My-Meta-Box script) which is 
  * a class for creating custom meta boxes for WordPress. 
  * 
- * @version 1.8.8
+ * @version 1.8.9
  * @copyright 2012 Ohad Raz 
  * @author Ohad Raz (email: admin@bainternet.info)
  * @link http://en.bainternet.info

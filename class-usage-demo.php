43aee27bc832968da7de67f7b9ef3202ce298608
raz ohad <admin@bainternet.info>
2012-10-25 20:11:15 +0300

    Update class-usage-demo.php
    updated version numer

--- class-usage-demo.php
+++ class-usage-demo.php
@@ -3,7 +3,7 @@
 Plugin Name: Demo Tax meta class
 Plugin URI: http://en.bainternet.info
 Description: Tax meta class usage demo
-Version: 1.9.4
+Version: 1.9.5
 Author: Bainternet, Ohad Raz
 Author URI: http://en.bainternet.info
 */
